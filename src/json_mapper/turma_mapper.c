e7f89d9e2fa50e6c4522382f112ce1b1ce701c69
XandersonSilva <isacsilvasouza2@gmail.com>
2026-04-03 15:56:44 -0300

    fix: Erro ao maper a entidade turma

--- src/json_mapper/turma_mapper.c
+++ src/json_mapper/turma_mapper.c
@@ -1,6 +1,8 @@
-#include "json_mapper/turma_mapper.h"
 #include <string.h>
 #include <stdlib.h>
+
+#include "utils/utils.h"
+#include "json_mapper/turma_mapper.h"
 
 cJSON *turma_to_json(void *obj)
 {
@@ -10,7 +12,14 @@ cJSON *turma_to_json(void *obj)
 
     cJSON_AddNumberToObject(json,"id",e->id);
     cJSON_AddStringToObject(json,"codigo",e->codigo);
-    
+    cJSON_AddNumberToObject(json,"docente_id",e->docente_id);
+    cJSON_AddNumberToObject(json,"qtd_matricula",e->qtd_matricula);
+
+    cJSON_AddItemToObject(
+        json,
+        "id_matricula",
+        int_array_to_json(e->id_matricula, e->qtd_matricula)
+    );
 
     return json;
 }
@@ -21,8 +30,18 @@ void *json_to_turma(cJSON *json)
 
     e->id = 
         cJSON_GetObjectItem(json, "id")->valueint;
+    
+    e->docente_id = 
+        cJSON_GetObjectItem(json, "docente_id")->valueint;
+    
+    e->qtd_matricula = 
+        cJSON_GetObjectItem(json, "qtd_matricula")->valueint;
+
     strcpy(e->codigo,
         cJSON_GetObjectItem(json,"codigo")->valuestring);
+    
+    cJSON *array = cJSON_GetObjectItem(json, "id_matricula");
+    e->id_matricula = json_to_int_array(array, e->qtd_matricula);
 
     return e;
 }

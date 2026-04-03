75941286563a1ece25d9bbb70def6a97beb76d46
XandersonSilva <isacsilvasouza2@gmail.com>
2026-04-03 16:05:10 -0300

    curso DAO criado

---
+++ include/dao/cursoDAO.h
@@ -0,0 +1,16 @@
+#ifndef CURSODAO_H
+#define CURSODAO_H
+
+
+#include "dao/jsonDAO.h"
+#include "json_mapper/curso_mapper.h"
+#include "model/curso.h"
+
+
+#define FILE "data/curso.json"
+
+void salvar_curso(Curso *d);
+
+Curso *buscar_curso(int id);
+
+#endif

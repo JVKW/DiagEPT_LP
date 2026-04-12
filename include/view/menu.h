28b7b059b5f91e674bb2a3b848e532a3ed1b61ea
XandersonSilva <isacsilvasouza2@gmail.com>
2026-04-12 15:38:57 -0300

    funções de mesmo escopo em um unico arquivo

--- include/view/menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef MENU_H
-#define MENU_H
-
-
-int mostrarMenu(const char *menu, int opc);
-void imprimirMenu(const char *menu);
-int lerOpcao();
-
-#endif
--- include/view/relatorio_evasao_view.h
+++
@@ -1,6 +0,0 @@
-#ifndef RELATORIO_EVASAO_VIEW
-#define RELATORIO_EVASAO_VIEW
-
-void relatorio_evasoes();
-
-#endif
